ad3efc26dcef9a39cd0c7c286887df42497d3d8a
bas <baspanupon84862@gmail.com>
2024-09-25 17:42:10 +0700

    about-me

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.css'
+  styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent {
-
+  
 }
